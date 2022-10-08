17c7709bae6ad9bb36f045ef1b2a2675f15815e3
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-10-08 23:49:42 +0200

    Update main.py

--- model/framework/grover/main.py
+++ model/framework/grover/main.py
@@ -6,6 +6,7 @@ import torch
 import pandas as pd
 from rdkit import RDLogger
 from pathlib import Path
+import tempfile
 
 from grover.util.parsing import parse_args, get_newest_train_args
 from grover.util.utils import create_logger
@@ -29,23 +30,17 @@ def setup(seed):
     torch.backends.cudnn.deterministic = True
 
 def smiles_to_dataframe(txt_file_path):
-
-    
     df = pd.read_csv(txt_file_path, header=None,  names=['smiles'])
-    
     dummy_labels = pd.Series(np.zeros(df.shape[0]))
-
     names =    ['E1-CC2','E2-CC2','f1-CC2','f2-CC2','E1-PBE0','E2-PBE0','f1-PBE0','f2-PBE0','E1-CAM','E2-CAM','f1-CAM','f2-CAM']
-    
     for n in names:
         df[n] = dummy_labels.values
-
     input_csv_path = txt_file_path.split(".")[0] + ".csv"
     df.to_csv(input_csv_path, index=False)
-    
     return input_csv_path
 
-    
+tmp_folder = tempfile.mktemp()
+features_path = os.path.join(tmp_folder, "features.npz")
 
 if __name__ == '__main__':
     # setup random seed
@@ -55,7 +50,7 @@ if __name__ == '__main__':
     # supress rdkit logger
     lg = RDLogger.logger()
     lg.setLevel(RDLogger.CRITICAL)
-
+    
     # Initialize MolVocab
     mol_vocab = MolVocab
 
@@ -67,13 +62,10 @@ if __name__ == '__main__':
     p = Path(s)
     model_path = str(p.parent.parent.absolute())
 
-    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/qm8', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/qm8/fold_0/model_0/model.pt', model_path+'/framework/finetune/qm8/fold_2/model_0/model.pt', model_path+'/framework/finetune/qm8/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=['features.npz'], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
-
-
-    sf.save_features_main(csv_path)
+    args = Namespace(batch_size=32, checkpoint_dir=model_path+'/framework/finetune/qm8', checkpoint_path=None, checkpoint_paths=[model_path+'/framework/finetune/qm8/fold_0/model_0/model.pt', model_path+'/framework/finetune/qm8/fold_2/model_0/model.pt', model_path+'/framework/finetune/qm8/fold_1/model_0/model.pt'], cuda=False, data_path=csv_path, ensemble_size=3, features_generator=None, features_path=[features_path], fingerprint=False, gpu=0, no_cache=True, no_features_scaling=True, output_path=output_path, parser_name='predict')
+    sf.save_features_main(csv_path,features_path)
 
     train_args = get_newest_train_args()
     avg_preds, test_smiles = make_predictions(args, train_args)
     write_prediction(avg_preds, test_smiles, args)
-
     os.remove(csv_path)
